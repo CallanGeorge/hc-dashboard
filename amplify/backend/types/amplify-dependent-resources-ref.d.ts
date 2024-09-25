52539419dddb3e5adf40bacbf68c93a94d01b49b
CallanGeorge <callangeorge19@gmail.com>
2024-09-25 14:26:33 +0100

    1 Added cognito Authentication

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "hcdashboard44dd8b42": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
